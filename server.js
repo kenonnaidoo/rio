f8d4587f1bbf25d042aba0efc40dcf062d1c9539
unknown <kenon.naidoo@gmail.com>
2021-02-20 00:54:53 +0200

    port

--- server.js
+++ server.js
@@ -26,4 +26,6 @@ io.on('connection', socket => {
     })
   })
 
-server.listen(3000)
+  app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
